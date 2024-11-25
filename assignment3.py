aaf05e89ce8b654ba793bd63c74e5178eb4dd4d1
pwanjiku <cikunjoroge4@gmail.com>
2024-11-25 10:49:45 +0300

    Update user prompt

--- assignment3.py
+++ assignment3.py
@@ -15,11 +15,20 @@
 # Conditionally render the discount
 # If the discount is >= 20%, apply it
 # If the discount <20%, return the original price
-def calculate_discount(price, discount):
-  new_discount = int(discount.strip('%'))
+def calculate_discount(price, discount='0'):
+  try:
+        new_discount = int(discount.strip('%'))
+  except ValueError:
+        return price
   if new_discount >= 20:
     return price - (price * new_discount // 100)
   else:
     return price
-print(calculate_discount(200, '50%'))
-print(calculate_discount(200, '10%'))
+
+# print(calculate_discount(200, '50%'))
+# print(calculate_discount(200, '10%'))
+# User Prompt1
+price = input('Enter the Price:')
+discount = input('Enter the Discount:')
+
+print(calculate_discount(int(price), discount))
