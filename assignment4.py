7bd9fef0e3d16a0ed7e7bd1e1a2646e3b918e53b
pwanjiku <cikunjoroge4@gmail.com>
2024-11-25 12:23:47 +0300

    Update user prompt

--- assignment4.py
+++ assignment4.py
@@ -2,3 +2,10 @@ with open('assignment1.py',"r") as file:
   content = file.read()
 with open('assignment.txt',"w") as file:
   file.write("Hello\n"+content)
+# User prompt
+file1 = input("File to rad from:")
+file2 = input("File to write to:")
+with open(file1,"r") as file:
+  content = file.read()
+with open(file2,"w") as file:
+  file.write("Hello\n"+content)
